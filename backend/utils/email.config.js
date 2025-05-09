58e490fb13fb8ae4a4f7c130a6ac7fb4e928c425
Payal <payalgupta425@gmail.com>
2025-05-09 22:23:52 +0530

    .env

--- backend/utils/email.config.js
+++ backend/utils/email.config.js
@@ -1,11 +1,12 @@
 import nodemailer from 'nodemailer'
-
+import dotenv from 'dotenv'
+dotenv.config()
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false, // true for port 465, false for other ports
     auth: {
       user: "payalgupta425@gmail.com",
-      pass: "opgh ikqj inby dhdh",
+      pass: "${process.env.PASSWORD}",
     },
 });
